d602e90fa011c4c5f95517ba3bbf583e33e0f9e0
samyeoubc <eileenyeo@live.ca>
2021-03-30 17:13:31 -0700

    Adding structure to reports

--- analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
+++ analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import os
 
-data = pd.read_excel('scripts/data.xlsx')
+os.chdir('C:/Users/eilee/course-project-solo_115')
+
+data = pd.read_excel(r'data/raw/data.xlsx')
 
 def load_and_process(data):
 
@@ -10,28 +13,28 @@ def load_and_process(data):
     .drop(columns=['locus_tag'])
     .rename(columns={"Name":"Gene Name", "gene":"Gene Symbol", "Stationary rep1":"Stationary Phase Group 1", 
                            "Stationary rep2":"Stationary Phase Group 2", "Stationary rep3":"Stationary Phase Group 3", 
-                          "3h fresh media rep1":"Growth in Media (3h) Group 1", "3h fresh media rep2":"Growth in Media (3h) Group 2",
-                          "3h fresh media rep3":"Growth in Media (3h) Group 3", "3h amp media rep1":"Media and Ampicillin (3h) Group 1",
-                          "3h amp media rep2":"Media and Ampicillin (3h) Group 2", "3h amp media rep3":"Media and Ampicillin (3h) Group 3"}))
+                          "3h fresh media rep1":"Growth in Media Group 1", "3h fresh media rep2":"Growth in Media Group 2",
+                          "3h fresh media rep3":"Growth in Media Group 3", "3h amp media rep1":"Media and Ampicillin Group 1",
+                          "3h amp media rep2":"Media and Ampicillin Group 2", "3h amp media rep3":"Media and Ampicillin Group 3"}))
 
     
     #function2
     col = df.loc[: , "Stationary Phase Group 1":"Stationary Phase Group 3"]
     df['Avg. Stationary Phase'] = col.mean(axis=1) 
 
-    col2 = df.loc[: , "Growth in Media (3h) Group 1":"Growth in Media (3h) Group 3"]
-    df['Avg. Growth in Media (3h)'] = col2.mean(axis=1) 
+    col2 = df.loc[: , "Growth in Media Group 1":"Growth in Media Group 3"]
+    df['Avg. Growth in Media'] = col2.mean(axis=1) 
 
-    col3 = df.loc[: , "Media and Ampicillin (3h) Group 1":"Media and Ampicillin (3h) Group 3"]
-    df['Avg. Media and Ampicillin (3h)'] = col3.mean(axis=1) 
+    col3 = df.loc[: , "Media and Ampicillin Group 1":"Media and Ampicillin Group 3"]
+    df['Avg. Media and Ampicillin'] = col3.mean(axis=1) 
 
 
     #function 3
     df2 = (pd.DataFrame(data=df)
-    .sort_values("Avg. Media and Ampicillin (3h)", ascending=False) 
+    .sort_values("Avg. Media and Ampicillin", ascending=False) 
     .drop(columns=["Stationary Phase Group 1", "Stationary Phase Group 2", "Stationary Phase Group 3", 
-                      "Growth in Media (3h) Group 1", "Growth in Media (3h) Group 2", "Growth in Media (3h) Group 3",
-                      "Media and Ampicillin (3h) Group 1", "Media and Ampicillin (3h) Group 2", "Media and Ampicillin (3h) Group 3"]))
+                      "Growth in Media Group 1", "Growth in Media Group 2", "Growth in Media Group 3",
+                      "Media and Ampicillin Group 1", "Media and Ampicillin Group 2", "Media and Ampicillin Group 3"]))
     return df2
 
 dataset = load_and_process(data)
